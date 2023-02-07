d3061fc332fed3e6ae91671e22ab5f3f05cafb89
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-02-07 14:49:29 +0100

    Screens

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text
+        style={{
+            // fontSize: 30, 
+            textAlign: 'center',
+            marginTop: 50,
+            color: 'black',
+            }}
+      >HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
